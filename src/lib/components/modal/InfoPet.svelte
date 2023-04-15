8c02ef6a16f9d178fe96b1c45de12cebd1888083
unknown <spicedexdee123@gmail.com>
2023-04-15 23:07:00 +0700

    :lipstick: : Update ui modal

--- src/lib/components/modal/InfoPet.svelte
+++ src/lib/components/modal/InfoPet.svelte
@@ -1,15 +1,38 @@
 <script>
-  export let pet;
+	export let pet;
 </script>
 
 <div class="p-4 flex flex-col gap-4 card">
-  <h1>{pet.name}</h1>
-  <div class="flex flex-col gap-4">
-    <div class="p-4">
-      <h2>Info</h2>
-    </div>
-    <div class="p-4">
-      <h2>Appointments</h2>
-    </div>
-  </div>
+	<h1>{pet.name}</h1>
+	<hr />
+	<div class="flex flex-col gap-4">
+		<div class="card p-4">
+			<section class="p-4">
+				<h3>ข้อมูลสัตว์เลี้ยง</h3>
+				<hr class="my-2" />
+				<div class="my-4 flex flex-col gap-y-5">
+					<label class="label">
+						<span>ชื่อ : </span>
+						<input class="input" type="text" value="{pet.name}" disabled>
+					</label>
+          <label class="label">
+						<span>สายพันธ์ุ : </span>
+						<input class="input" type="text" value="สายพันธ์ุ" disabled>
+					</label>
+          <label class="label">
+            <span>เพศ : </span>
+            <input class="input" type="text" value="" disabled>
+          </label>
+          <label>
+            <span>วันเกิด : </span>
+            <input class="input" type="date" value="" disabled>
+          </label>
+          <label class="label">
+            <span>รายละเอียดเพิ่มเติม : </span>
+            <textarea class="textarea" rows="4" placeholder="รายละเอียดเพิ่มเกี่ยวกัยสัตว์" />
+          </label>
+				</div>
+			</section>
+		</div>
+	</div>
 </div>
