76aefad4430ac1736cb85f4f3bb545c92dff7c21
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2023-04-15 22:39:27 +0700

    :poop:  : Modal Pet

---
+++ src/lib/components/modal/InfoPet.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let pet;
+</script>
+
+<div class="p-4 flex flex-col gap-4 card">
+  <h1>{pet.name}</h1>
+  <div class="flex flex-col gap-4">
+    <div class="p-4">
+      <h2>Info</h2>
+    </div>
+    <div class="p-4">
+      <h2>Appointments</h2>
+    </div>
+  </div>
+</div>
--- src/routes/pets/+page.svelte
+++ src/routes/pets/+page.svelte
@@ -1,15 +1,43 @@
 <script>
 	/** @type {import('./$types').PageData} */
 	export let data;
+	import { modalStore } from '@skeletonlabs/skeleton';
+    import InfoPet from '$lib/components/modal/InfoPet.svelte';
+	const fakeData = [
+		{
+			id: 1,
+			name: 'Pet name',
+			type: 'Dog',
+			breed: 'Breed'
+		},
+        {
+            id: 2,
+            name: 'Pet name 2',
+            type: 'Cat',
+            breed: 'Breed 2'
+        }
+	];
 
-    
+	const openModalPet = (pet) => {
+		modalStore.trigger({
+			type: 'component',
+			component: {
+				ref: InfoPet,
+				props: {
+					pet
+				}
+			}
+		});
+	};
 </script>
 
 <div class="p-4 flex flex-col gap-4">
 	<h1>Pets</h1>
-    <div class="flex gap-4">
-        <div class="card card-hover cursor-pointer p-4 w-1/4">
-            <h2>Pet name</h2>
-        </div>
-    </div>
+	<div class="flex gap-4">
+		{#each fakeData as stillFakeData}
+			<button on:click={() => {openModalPet(stillFakeData)}} class="card card-hover cursor-pointer p-4 w-1/4">
+				<h2>{stillFakeData.name}</h2>
+			</button>
+		{/each}
+	</div>
 </div>
